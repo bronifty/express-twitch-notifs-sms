88d8f4aea60ad0763762bf0120130883cc6567de
bronifty <bronifty@gmail.com>
2022-09-27 14:24:31 -0400

    .

--- utils/curl.js
+++ utils/curl.js
@@ -1,6 +1,4 @@
-curl -X POST 'https://api.twitch.tv/helix/eventsub/subscriptions' \
--H 'Authorization: Bearer 2gbdx6oar67tqtcmt49t3wpcgycthx' \
--H 'Client-Id: wbmytr93xzw8zbg0p1izqyzzc5mbiz' \
--H 'Content-Type: application/json' \
--d '{"type":"channel.follow","version":"1","condition":{"broadcaster_user_id":"1234"},"transport":{"method":"webhook","callback":"https://example.com/callback","secret":"s3cre77890ab"}}' 
+curl -X GET 'https://api.twitch.tv/helix/eventsub/subscriptions' \
+-H 'Authorization: Bearer 6f04qk75neyroremydvo8tjd6mq6pn' \
+-H 'Client-Id: 0p0vu5twxkvyt6fkukwdi7nf6b0wus'
 

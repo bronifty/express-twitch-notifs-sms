c2f3397634308104d8c3b2589ff42b41d7c567db
bronifty <bronifty@gmail.com>
2022-09-27 14:58:18 -0400

    .

--- utils/checkSubs.js
+++ utils/checkSubs.js
@@ -5,8 +5,14 @@ import loadTwitchTokenFromRedis from '../utils/loadTwitchTokenFromRedis.js';
 // load the twitch_access_token from redis and use it to fetch data from twitch
 export const checkSubs = async (req, res) => {
   let twitch_access_token = await loadTwitchTokenFromRedis();
+  console.log(
+    `
+    Authorization: Bearer ${twitch_access_token}
+    Client-Id: ${process.env.TWITCH_CLIENT_ID_TEST}
+    `
+  );
   const promiseData = await fetch(
-    'https://api.twitch.tv/helix/eventsub/subscriptions',
+    `https://api.twitch.tv/helix/eventsub/subscriptions`,
     {
       method: 'GET',
       headers: {
--- utils/curl.js
+++ utils/curl.js
@@ -2,3 +2,4 @@ curl -X GET 'https://api.twitch.tv/helix/eventsub/subscriptions' \
 -H 'Authorization: Bearer 6f04qk75neyroremydvo8tjd6mq6pn' \
 -H 'Client-Id: 0p0vu5twxkvyt6fkukwdi7nf6b0wus'
 
+TWITCH_SUBSCRIPTION_SECRET_PHRASE

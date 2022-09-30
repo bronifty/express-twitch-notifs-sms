e5317ca378cf577a9583088d96e330c3ab454d03
bronifty <bronifty@gmail.com>
2022-09-30 19:01:24 -0400

    .

---
+++ utils/swagger.ts
@@ -0,0 +1,4 @@
+import { Express, Request, Response } from 'express';
+import swaggerJsdoc from 'swagger-jsdoc'; // docs
+import swaggerUi from 'swagger-ui-express'; // interface
+import { version } from '../package.json';
